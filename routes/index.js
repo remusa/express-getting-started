ec9a17734590e153cc4ccfd5597391934f91d8c9
R Sánchez <rene.as.himself@gmail.com>
2020-05-06 12:36:26 -0500

    Basic routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router()
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Hello World!' })
+})
+
+router.post('/', (req, res) => {
+  res.send('Got a POST request')
+})
+
+router.put('/', (req, res) => {
+  res.send('Got a PUT request')
+})
+
+router.delete('/', (req, res) => {
+  res.send('Got a DELETE request')
 })
 
 module.exports = router
